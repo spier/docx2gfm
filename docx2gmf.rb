a436e56e5fad4c7cf67053c697633cfb756243c2
Sebastian Spier <sebastian.spier@meltwater.com>
2018-12-21 19:08:57 +0100

    Remove commonmarker dependency, and function move_links_to_the_end that used commonmarker. Using plain regex to move the links to the end of the markdown file now.

--- docx2gmf.rb
+++ docx2gmf.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/ruby
 
 require "pp"
-require "commonmarker"
 
 if ARGV.length < 1
   puts "Too few arguments."
@@ -51,7 +50,7 @@ def clean_link_placeholder(text)
   text.downcase.gsub(/\s/,'-')
 end
 
-def move_links_to_the_end_regex(content)
+def move_links_to_the_end(content)
   # matcher = content.scan(/[^!]\[(?<text>.*?)\]\((?<url>.*?)\)/)
   # TODO using named groups below would be more descriptive. Need to figure out how.
 
@@ -74,42 +73,6 @@ def move_links_to_the_end_regex(content)
   return content
 end
 
-def convert_to_reference_style_links(content)
-  # parse the files specified on the command line
-  doc = CommonMarker.render_doc(content)
-
-  dictionary = {}
-
-  # Walk tree and print out URLs for links
-  doc.walk do |node|
-    if node.type == :link
-      # printf("text =  %s, URL = %s\n", node.first_child().string_content, node.url)
-      unless (dictionary.has_key?(node.url))
-        dictionary[node.url] = clean_link_placeholder(node.first_child().string_content)
-      end
-
-      node.first_child.string_content = "[#{node.first_child.string_content}][#{dictionary[node.url]}]"
-      node.insert_before(node.first_child)
-      node.delete
-    end
-  end
-
-  content = doc.to_commonmark
-
-  # Cleaning up the messy doc.to_commonmark creates references like this:
-  # \[text\]\[reference\] => [text][reference]
-  # TODO is it really true that backslash (\) does not appear in the text. dangerous move!!
-  content = content.gsub(/\\(\[|\])/,"\\1")
-
-  # add references to the end of the content
-  content += "\n"
-  dictionary.each_pair do |url, label|
-      content += "[#{label}]: #{url}\n"
-  end
-
-  return content
-end
-
 # convert docx to initial markdown
 def docx_2_markdown(docx_filename)
   # TODO check out pandoc options that might be useful e.g. --extract-media='/images/own/'
@@ -125,7 +88,7 @@ docx_filename = ARGV[0]
 
 content = docx_2_markdown(docx_filename)
 content = cleanup_content(content)
-content = move_links_to_the_end_regex(content)
+content = move_links_to_the_end(content)
 content = add_frontmatter(content)
 
 puts content
